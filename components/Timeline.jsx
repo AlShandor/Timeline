5cf63259faa6004f78efc3e6d5329e080f4ab55e
Alexander Atanasov <alexander.atanasoff@gmail.com>
2024-01-19 14:45:54 +0200

    refactor Timeline component

--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -1,17 +1,17 @@
 "use client"
 
-import React from 'react';
+import { useRef, useEffect, cloneElement } from 'react';
 import TL from '@utilities/timeline3/js/TL';
 import '@styles/timeline.css';
 
 const Timeline = ({ target, events, title, options }) => {
-    const timelineEl = React.useRef(null);
-    React.useEffect(() => {
+    const timelineEl = useRef(null);
+    useEffect(() => {
         if (timelineEl.current) {
             new TL.Timeline(timelineEl.current, { events, title, options });
         }
     }, []);
-    return React.cloneElement(target, { ref: timelineEl });
+    return cloneElement(target, { ref: timelineEl });
 };
 
 export default Timeline;
