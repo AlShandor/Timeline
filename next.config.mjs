b954ad3aebb4b0a150e922ab575f1133080ac9fe
Alexander Atanasov <alexander.atanasoff@gmail.com>
2024-11-21 15:03:25 +0200

    add britannica site to allowed image sources

--- next.config.mjs
+++ next.config.mjs
@@ -51,6 +51,12 @@ const nextConfig = {
 				port: "",
 				pathname: "/**",
 			},
+			{
+				protocol: "https",
+				hostname: "cdn.britannica.com",
+				port: "",
+				pathname: "/**",
+			},
 		],
 	},
 };
